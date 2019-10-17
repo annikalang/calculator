dc323bef355cc2228a5617c5ff53b7e8c865f7de
Annika Lang <Lang-annika@t-online.de>
2019-10-17 21:14:43 +0200

    added JS functions to get the history and the output

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-script.js
+function getHistory(){
+  return document.getElementById("history-value").innerText;
+}
+
+function printHistory(num){
+  document.getElementById("history-value").innerText=num;
+}
+
+function getOutput(){
+  return document.getElementById("output-value").innerText;
+}
+
+function printOutput(num){
+  if(num==""){
+    document.getElementById("output-value").innerText=num;
+  }
+  else{
+  document.getElementById("output-value").innerText=getFormattedNumber(num);
+  }
+}
+
+function getFormattedNumber(num){
+  const n = Number(num);
+  const value = n.toLocaleString("en");
+  return value
+}
+
+function reverseNumberFormat(num){
+  return Number(num.replace(/,/g,''))
+}
+
+alert(reverseNumberFormat(getOutput()));
