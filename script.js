8051fe5cd8ae529ce9c03cf3840970fa1347c7ef
Annika Lang <Lang-annika@t-online.de>
2019-10-19 15:51:12 +0200

    added further calculator JS logic, history and output

--- script.js
+++ script.js
@@ -20,8 +20,8 @@ function printOutput(num){
 }
 
 function getFormattedNumber(num){
-  const n = Number(num);
-  const value = n.toLocaleString("en");
+  let n = Number(num);
+  let value = n.toLocaleString("en");
   return value
 }
 
@@ -32,10 +32,41 @@ function reverseNumberFormat(num){
 
 
 
-const operator = document.getElementsByClassName("operator")
-for(const i = 0; i < operator.length; i++){
+let operator = document.getElementsByClassName("operator")
+for(let i = 0; i < operator.length; i++){
   operator[i].addEventListener('click', function(){
-    alert("The user clicked:" + this.id);
+    // alert("The operator clicked:" + this.id);
+    if(this.id == "clear"){
+      printHistory("");
+      printOutput("");
+    }
+    if(this.id == "backspace"){
+      let output = reverseNumberFormat(getOutput()).toString();
+      if(output){ // if output has a value
+        output = output.substr(0,output.length-1);
+        printOutput(output);
+
+      }
+    }
+    else{
+      let output = getOutput();
+      let history = getHistory();
+      if(output != ""){
+        output = reverseNumberFormat(output);
+      }
+    }
   })
+}
+
+let number = document.getElementsByClassName("number")
+for(let i = 0; i < number.length; i++){
+  number[i].addEventListener('click', function(){
+    // alert("The number clicked:" + this.id);
+    let output = reverseNumberFormat(getOutput());
+    if(output! = NaN) { // if number is a number
+      output = output + this.id;
+      printOutput(output);
+    }
+  });
 }
 
